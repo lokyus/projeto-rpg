e88578bce16986ed171f45ddbe37de90b07f58ed
lokyus <lokyus@gmail.com>
2026-04-15 22:23:39 -0300

    Minha primeira alteração no projeto de RPG

---
+++ script.js
@@ -0,0 +1,8 @@
+function rolarDados() {
+    // Gera um número aleatório entre 1 e 20 (tipo um D20 de RPG)
+    let forca = Math.floor(Math.random() * 20) + 1;
+    let agilidade = Math.floor(Math.random() * 20) + 1;
+
+    document.getElementById("forca").innerText = forca;
+    document.getElementById("agilidade").innerText = agilidade;
+}
